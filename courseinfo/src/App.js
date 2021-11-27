31f941cfd98c87b0afd66e6a421dc6dd82379722
pyrhaa <sboubtane@protonmail.com>
2021-11-27 15:28:55 +0100

    use log to catch array in the props object

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -5,6 +5,8 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
+  const m1 = props.parts;
+  console.log(m1);
   return (
     <>
       <p>
@@ -15,11 +17,10 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  // console.log(props);
   return (
     <div>
-      <Part name={props.name1} exercises={props.exercises1} />
-      <Part name={props.name2} exercises={props.exercises2} />
-      <Part name={props.name3} exercises={props.exercises3} />
+      <Part parts={props.parts} />
     </div>
   );
 };
@@ -35,35 +36,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        name1={part1.name}
-        exercises1={part1.exercises}
-        name2={part2.name}
-        exercises2={part2.exercises}
-        name3={part3.name}
-        exercises3={part3.exercises}
-      />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
