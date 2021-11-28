e9a3d8e5b58363fd3efac2ddf37fed3b5d0d6769
pyrhaa <sboubtane@protonmail.com>
2021-11-28 18:04:03 +0100

    courseinfo end

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -4,6 +4,8 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+// arrayPart : catch the array parts in the props object
+// parts : map to arrayPart each element name and exercices in a list
 const Part = (props) => {
   const arrayPart = props.parts;
 
@@ -24,6 +26,8 @@ const Content = (props) => {
   );
 };
 
+// exercices : catch all exercices properties from array parts in props object
+// [first, second, third] : take each exercices number values individualy
 const Total = (props) => {
   const exercices = props.parts.map((el) => el.exercises);
   const [first, second, third] = exercices;
