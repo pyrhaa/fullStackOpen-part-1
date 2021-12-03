5e4df46887245f06f41a8b4bb6bf39c006e9b2af
pyrhaa <sboubtane@protonmail.com>
2021-12-03 15:04:21 +0100

    res all works

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -8,25 +8,37 @@ const Title = () => {
   return <h2>statistics</h2>;
 };
 
+//display click incrementation result for each props
 const Statistics = (props) => {
+  const good = props.good;
+  const neutral = props.neutral;
+  const bad = props.bad;
+  const all = good + neutral + bad;
+
   return (
     <>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average</p>
+      <p>positive %</p>
     </>
   );
 };
 
+//button with event handler click and text
 const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   return (
     <div>
@@ -35,7 +47,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Title />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   );
 };
